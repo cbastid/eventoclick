3fd0ac6fda455be94b7ac6aebdc0dffe3be55cb2
Cristian Bastidas <cbastid.dr@gmail.com>
2022-10-17 17:44:46 -0300

    ejercicio evento onclick

---
+++ evento.js
@@ -0,0 +1,10 @@
+function fnlogin(elemento) {
+    // console.log("elemento clickeado", elemento);
+    if(elemento.innerText == "Login"){
+        elemento.innerText="Logout"
+    }else{
+        elemento.innerText="Login"
+    }
+   
+}
+
